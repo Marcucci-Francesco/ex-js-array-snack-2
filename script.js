fb9c0daa491c3e3a95d36f4d9d487a8da96845d9
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-05-12 15:48:33 +0200

    [FEAT] add bonus exercise 5-7

--- script.js
+++ script.js
@@ -73,3 +73,38 @@ authors.sort((a, b) => a.age - b.age) * (areAuthorsAdults ? 1 : -1);
 const ages = books.map(b => b.author.age);
 const agesSum = ages.reduce((sum, age) => sum + age, 0);
 console.log(`The average age of the authors is: ${agesSum / ages.length}`);
+
+
+// Snack Bonus 5
+const ids = [2, 13, 7, 21, 19];
+async function getBooks(ids) {
+  const baseUrl = `https://freetestapi.com/api/v1/books/`;
+  const bookPromises = ids.map(id => fetch(`${baseUrl}${id}`).then(r => r.json()));
+  const books = await Promise.all(bookPromises);
+  return books;
+}
+
+getBooks(ids).then(books => console.log(books));
+
+
+// Snack Bonus 6
+const areThereAvailableBooks = books.some(b => b.avalaible);
+const booksByPrice = books.sort((a, b) => {
+  const priceA = parseFloat(a.price.replace('€', ''))
+  const priceB = parseFloat(b.price.replace('€', ''))
+  return priceA - priceB;
+})
+booksByPrice.sort((a, b) => a.available === b.available ? 0 : a.available ? -1 : 1);
+
+
+// Snack Bonus 7
+const booksWithTags = books.reduce((acc, book) => {
+  book.tags.forEach(tag => {
+    if (!acc[tag]) {
+      acc[tag]++;
+    } else {
+      acc[tag] = 1
+    }
+  })
+  return acc;
+}, {});
